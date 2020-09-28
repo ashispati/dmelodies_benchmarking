9ecfaf9db77b1778d09d6cff43e012fe82041ada
Ashis Pati <ashis.pati@gatech.edu>
2020-09-28 11:06:19 -0400

    refactor to run with cnn-based vae

--- script_train_dmelodies.py
+++ script_train_dmelodies.py
@@ -3,12 +3,15 @@ import json
 import torch
 from dmelodies_torch_dataloader import DMelodiesTorchDataset
 from src.dmelodiesvae.dmelodies_vae import DMelodiesVAE
+from src.dmelodiesvae.dmelodies_cnnvae import DMelodiesCNNVAE
 from src.dmelodiesvae.dmelodies_vae_trainer import DMelodiesVAETrainer
+from src.dmelodiesvae.dmelodies_cnnvae_trainer import DMelodiesCNNVAETrainer
 
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_type", type=str, default='beta-VAE', choices=['beta-VAE', 'annealed-VAE', 'ar-VAE'])
+parser.add_argument("--net_type", type=str, default='rnn', choices=['rnn', 'cnn'])
 parser.add_argument("--gamma", type=float, default=1.0)
 parser.add_argument("--delta", type=float, default=10.0)
 
@@ -17,6 +20,16 @@ args = parser.parse_args()
 # Select the Type of VAE-model
 m = args.model_type
 
+# Specify the network and trainer classes
+net_type = args.net_type
+if net_type == 'cnn':
+    model = DMelodiesCNNVAE
+    trainer = DMelodiesCNNVAETrainer
+else:
+    model = DMelodiesVAE
+    trainer = DMelodiesVAETrainer
+
+# Specify training params
 seed_list = [0, 1, 2]
 model_dict = {
     'beta-VAE': {
@@ -29,7 +42,7 @@ model_dict = {
     },
     'ar-VAE': {
         'capacity_list': [50.0],
-        'beta_list': [0.001],
+        'beta_list': [0.2],
         'gamma': args.gamma,
         'delta': args.delta,
     }
@@ -37,13 +50,14 @@ model_dict = {
 num_epochs = 100
 batch_szie = 512
 
+
 c_list = model_dict[m]['capacity_list']
 b_list = model_dict[m]['beta_list']
 for seed in seed_list:
     for c in c_list:
         for b in b_list:
             dataset = DMelodiesTorchDataset(seed=seed)
-            vae_model = DMelodiesVAE(dataset)
+            vae_model = model(dataset)
             if torch.cuda.is_available():
                 vae_model.cuda()
             trainer_args = {
@@ -56,15 +70,15 @@ for seed in seed_list:
             if m == 'ar-VAE':
                 trainer_args.update({'gamma': model_dict[m]['gamma']})
                 trainer_args.update({'delta': model_dict[m]['delta']})
-            trainer = DMelodiesVAETrainer(
+            vae_trainer = trainer(
                 dataset,
                 vae_model,
                 **trainer_args
             )
             if not os.path.exists(vae_model.filepath):
-                trainer.train_model(batch_size=batch_szie, num_epochs=num_epochs, log=True)
+                vae_trainer.train_model(batch_size=batch_szie, num_epochs=num_epochs, log=True)
             else:
                 print('Model exists. Running evaluation.')
-            trainer.load_model()
-            metrics = trainer.compute_eval_metrics()
+            vae_trainer.load_model()
+            metrics = vae_trainer.compute_eval_metrics()
             print(json.dumps(metrics, indent=2))
--- src/dmelodiesvae/dmelodies_cnnvae_trainer.py
+++ src/dmelodiesvae/dmelodies_cnnvae_trainer.py
@@ -11,8 +11,12 @@ class DMelodiesCNNVAETrainer(DMelodiesVAETrainer):
             lr=1e-4,
             # reg_type: Tuple[str] = None,
             beta=0.001,
+            gamma=1.0,
+            delta=10.0,
             capacity=0.0,
             device=0,
             rand=0,
     ):
-        super(DMelodiesCNNVAETrainer, self).__init__(dataset, model, model_type, lr, beta, capacity, device, rand)
+        super(DMelodiesCNNVAETrainer, self).__init__(
+            dataset, model, model_type, lr, beta, gamma, delta, capacity, device, rand
+        )
